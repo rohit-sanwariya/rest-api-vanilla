6641173d6fc482587df48acf6a8c3659ac23a845
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2020-11-18 16:35:32 +0530

    "Json rendered with writeHead write"

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http');
-
+const product = require('./data/product.json')
 const server = http.createServer((req,res)=>{
     console.log(123);
-    res.statusCode = 200;
-    res.setHeader("Content-Type","text/html")
-    res.write(`<h1>Hello world</h1>`);
+    // res.statusCode = 200;
+    // res.setHeader("Content-Type","text/html")
+    // res.write(`<h1>Hello world</h1>`);
+
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify(product))
+  
+
+
+
     res.end()
 });
 
